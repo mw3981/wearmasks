174ee9f3e825871df7115672bc207790efdce5e3
michelle <michelle@Michelles-MacBook-Pro-2.local>
2020-12-07 23:13:33 -0500

    This is my commit message

---
+++ script.js
@@ -0,0 +1,21 @@
+ScrollReveal().reveal('.morebackgrnd', { delay: 500, reset: true  });
+ScrollReveal().reveal('.facts', { delay: 800, reset: true  });
+ScrollReveal().reveal('.politics', { delay: 500, reset: true  });
+ScrollReveal().reveal('.fakenews', { delay: 500, reset: true  });
+ScrollReveal().reveal('.atlas', { delay: 500, reset: true  });
+ScrollReveal().reveal('.tweetimg', { delay: 800, reset: true  });
+ScrollReveal().reveal('.atlastweet', { delay: 800, reset: true  });
+ScrollReveal().reveal('.posters', { delay: 800, reset: true  });
+
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.fakenews2', {
+  rotate: { x: 100, y: 100, z: 0 },
+  duration: 3000
+});
+
+sr.reveal('.section4', {
+  rotate: { x: 100, y: 100, z: 0 },
+  duration: 3000
+});
